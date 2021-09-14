4cf061f1c091c30af49f5ad98c84e1caec84885e
Manfred Mueller <mail@manfred-mueller.org>
2021-09-14 15:33:17 +0200

    Saved settings are encrypted now.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,75 @@
 using System.IO;
 using System.Diagnostics;
 using Microsoft.Deployment.WindowsInstaller;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace signversioned
 {
+    public class Encryption
+    {
+    public static string Encrypt(string textToEncrypt)
+    {
+        try
+        {
+            string ToReturn = "";
+            string publickey = "52830761";
+            string secretkey = "Cfg_!7KjH";
+            byte[] secretkeyByte = { };
+            secretkeyByte = Encoding.UTF8.GetBytes(secretkey);
+            byte[] publickeybyte = { };
+            publickeybyte = Encoding.UTF8.GetBytes(publickey);
+            MemoryStream ms = null;
+            CryptoStream cs = null;
+            byte[] inputbyteArray = Encoding.UTF8.GetBytes(textToEncrypt);
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+            {
+                ms = new MemoryStream();
+                cs = new CryptoStream(ms, des.CreateEncryptor(publickeybyte, secretkeyByte), CryptoStreamMode.Write);
+                cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                cs.FlushFinalBlock();
+                ToReturn = Convert.ToBase64String(ms.ToArray());
+            }
+            return ToReturn;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message, ex.InnerException);
+        }
+    }
+
+    public static string Decrypt(string textToDecrypt)
+    {
+        try
+        {
+            string ToReturn = "";
+            string publickey = "52830761";
+            string privatekey = "Cfg_!7KjH";
+            byte[] privatekeyByte = { };
+            privatekeyByte = Encoding.UTF8.GetBytes(privatekey);
+            byte[] publickeybyte = { };
+            publickeybyte = Encoding.UTF8.GetBytes(publickey);
+            MemoryStream ms = null;
+            CryptoStream cs = null;
+            byte[] inputbyteArray = new byte[textToDecrypt.Replace(" ", "+").Length];
+            inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+            {
+                ms = new MemoryStream();
+                cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte), CryptoStreamMode.Write);
+                cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                cs.FlushFinalBlock();
+                Encoding encoding = Encoding.UTF8;
+                ToReturn = encoding.GetString(ms.ToArray());
+            }
+            return ToReturn;
+        }
+        catch (Exception ae)
+        {
+            throw new Exception(ae.Message, ae.InnerException);
+        }
+    }
+}
     public class MsiProperties
     {
         public static string Get(string msi, string name)
@@ -44,12 +110,6 @@ namespace signversioned
                 return;
             }
 
-            if (string.IsNullOrEmpty(Properties.Settings.Default.SignedBy) || string.IsNullOrEmpty(Properties.Settings.Default.TimeServer))
-            {
-                Console.WriteLine(Properties.Resources.ErrorIssuerAndOrTimeServerSettingsNotFound);
-                return;
-            }
-
             if (args.Length != 1)
             {
                 Console.WriteLine(Properties.Resources.UsageSignversionedExeTarget);
@@ -64,14 +124,20 @@ namespace signversioned
             if (args[0].Equals("params"))
             {
                 Console.WriteLine(Properties.Resources.PleaseKeyInTheSignerSNameN);
-                Properties.Settings.Default.SignedBy = Console.ReadLine();
+                Properties.Settings.Default.SignedBy = Encryption.Encrypt(Console.ReadLine());
                 Properties.Settings.Default.Save();
                 Console.WriteLine(Properties.Resources.NPleaseKeyInTheTimeServerN);
-                Properties.Settings.Default.TimeServer = Console.ReadLine();
+                Properties.Settings.Default.TimeServer = Encryption.Encrypt(Console.ReadLine());
                 Properties.Settings.Default.Save();
                 Console.WriteLine(Properties.Resources.NPleaseKeyInAdditionalParametersEnterForNoneN);
-                Properties.Settings.Default.AdditionalParams = Console.ReadLine();
+                Properties.Settings.Default.AdditionalParams = Encryption.Encrypt(Console.ReadLine());
                 Properties.Settings.Default.Save();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.SignedBy) || string.IsNullOrEmpty(Properties.Settings.Default.TimeServer))
+            {
+                Console.WriteLine(Properties.Resources.ErrorIssuerAndOrTimeServerSettingsNotFound);
                 return;
             }
 
@@ -79,9 +145,9 @@ namespace signversioned
             string vPath = path;
             string signString = "sign";
 
-            signString += " /n " + Properties.Settings.Default.SignedBy;
-            signString += " /t " + Properties.Settings.Default.TimeServer;
-            signString += " " + Properties.Settings.Default.AdditionalParams;
+            signString += " /n " + Encryption.Decrypt(Properties.Settings.Default.SignedBy);
+            signString += " /t " + Encryption.Decrypt(Properties.Settings.Default.TimeServer);
+            signString += " " + Encryption.Decrypt(Properties.Settings.Default.AdditionalParams);
             signString += " " + args[0];
             Process signProc = new Process();
             ProcessStartInfo signProcStartInfo = new ProcessStartInfo();
