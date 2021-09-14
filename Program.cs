f51c63814a9342e883da0ca0d9a6137b5f545da1
Manfred Mueller <mail@manfred-mueller.org>
2021-09-14 17:00:55 +0200

    Encryption logic outsourced in Encryption.cs

--- Program.cs
+++ Program.cs
@@ -2,75 +2,9 @@
 using System.IO;
 using System.Diagnostics;
 using Microsoft.Deployment.WindowsInstaller;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace signversioned
 {
-    public class Encryption
-    {
-    public static string Encrypt(string textToEncrypt)
-    {
-        try
-        {
-            string ToReturn = "";
-            string publickey = "52830761";
-            string secretkey = "Cfg_!7KjH";
-            byte[] secretkeyByte = { };
-            secretkeyByte = Encoding.UTF8.GetBytes(secretkey);
-            byte[] publickeybyte = { };
-            publickeybyte = Encoding.UTF8.GetBytes(publickey);
-            MemoryStream ms = null;
-            CryptoStream cs = null;
-            byte[] inputbyteArray = Encoding.UTF8.GetBytes(textToEncrypt);
-            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
-            {
-                ms = new MemoryStream();
-                cs = new CryptoStream(ms, des.CreateEncryptor(publickeybyte, secretkeyByte), CryptoStreamMode.Write);
-                cs.Write(inputbyteArray, 0, inputbyteArray.Length);
-                cs.FlushFinalBlock();
-                ToReturn = Convert.ToBase64String(ms.ToArray());
-            }
-            return ToReturn;
-        }
-        catch (Exception ex)
-        {
-            throw new Exception(ex.Message, ex.InnerException);
-        }
-    }
-
-    public static string Decrypt(string textToDecrypt)
-    {
-        try
-        {
-            string ToReturn = "";
-            string publickey = "52830761";
-            string privatekey = "Cfg_!7KjH";
-            byte[] privatekeyByte = { };
-            privatekeyByte = Encoding.UTF8.GetBytes(privatekey);
-            byte[] publickeybyte = { };
-            publickeybyte = Encoding.UTF8.GetBytes(publickey);
-            MemoryStream ms = null;
-            CryptoStream cs = null;
-            byte[] inputbyteArray = new byte[textToDecrypt.Replace(" ", "+").Length];
-            inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
-            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
-            {
-                ms = new MemoryStream();
-                cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte), CryptoStreamMode.Write);
-                cs.Write(inputbyteArray, 0, inputbyteArray.Length);
-                cs.FlushFinalBlock();
-                Encoding encoding = Encoding.UTF8;
-                ToReturn = encoding.GetString(ms.ToArray());
-            }
-            return ToReturn;
-        }
-        catch (Exception ae)
-        {
-            throw new Exception(ae.Message, ae.InnerException);
-        }
-    }
-}
     public class MsiProperties
     {
         public static string Get(string msi, string name)
